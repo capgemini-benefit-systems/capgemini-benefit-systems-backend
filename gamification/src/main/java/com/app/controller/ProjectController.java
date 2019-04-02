98a891fca46330f7d0d5fb1efae9766810697d22
jderen <kuba35groszy@tlen.pl>
2019-04-02 21:38:25 +0200

    findAll() in ProjectController

--- gamification/src/main/java/com/app/controller/ProjectController.java
+++ gamification/src/main/java/com/app/controller/ProjectController.java
@@ -1,10 +1,12 @@
 package com.app.controller;
 
 
+import com.app.model.Account;
 import com.app.model.Project;
-import com.app.model.dao.ActivityDao;
-import com.app.model.dao.ProjectDao;
+import com.app.model.User;
+import com.app.model.dao.*;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,17 +18,25 @@ public class ProjectController {
 
     private final ProjectDao projectDao;
     private final ActivityDao activityDao;
+    private final ProjectMembersDao projectMembersDao;
+    private final UserDao userDao;
+    private final AccountDao accountDao;
 
     @Autowired
-    public ProjectController(ProjectDao projectDao, ActivityDao activityDao) {
+    public ProjectController(ProjectDao projectDao, ActivityDao activityDao, ProjectMembersDao projectMembersDao, UserDao userDao, AccountDao accountDao) {
         this.projectDao = projectDao;
         this.activityDao = activityDao;
+        this.projectMembersDao = projectMembersDao;
+        this.userDao = userDao;
+        this.accountDao = accountDao;
     }
 
     @RequestMapping("/all")
     public List<Project> findAll(){
     return projectDao.findAll();
     }
+
+
 
 
 }
