8d27147023ae0ebd6cb79909b7f905961cfe5e74
jderen <kuba35groszy@tlen.pl>
2019-04-17 11:47:19 +0200

    UserDto and User converter

--- gamification/src/main/java/com/app/model/dto/UserDto.java
+++ gamification/src/main/java/com/app/model/dto/UserDto.java
@@ -1,9 +1,17 @@
 package com.app.model.dto;
 
+import com.app.model.User;
+import com.app.model.dao.AccountDao;
+import com.app.model.dao.AccountDaoImpl;
+import com.app.model.dao.UserDao;
+import com.app.model.dao.UserDaoImpl;
+import com.app.model.enums.Role;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
+import java.util.ArrayList;
 
 @Data
 @NoArgsConstructor
@@ -18,4 +26,40 @@ public class UserDto {
     private Long pointsSum;
     private Long currentPoints;
     private Long accountId;
+
+    /*todo*/
+    /*private Long transactionId;
+    private Long activityResultId;*/
+
+
+    public static UserDto getUserDtoByUser(User modelUser){
+        return UserDto.builder()
+                .id(modelUser.getId())
+                .email(modelUser.getEmail())
+                .name(modelUser.getName())
+                .surname(modelUser.getSurname())
+                .role(modelUser.getRole().name())
+                .pointsSum(modelUser.getPointsSum())
+                .currentPoints(modelUser.getCurrentPoints())
+                .accountId(modelUser.getAccount().getId())
+                .build();
+    }
+
+    public static User getUserByUserDto(UserDto userDto){
+        AccountDao accountDao=new AccountDaoImpl();
+        return User.builder()
+                .id(userDto.getId())
+                .email(userDto.getEmail())
+                .name(userDto.getName())
+                .surname(userDto.getSurname())
+                .role(Role.valueOf(userDto.getRole()))
+                .pointsSum(userDto.getPointsSum())
+                .currentPoints(userDto.getCurrentPoints())
+                .account(accountDao.findById(userDto.getAccountId()).orElseThrow(NullPointerException::new))
+                .transactions(new ArrayList<>()/*todo*/)
+                .activityResults(new ArrayList<>()/*todo*/)
+                .build();
+
+    }
+
 }
