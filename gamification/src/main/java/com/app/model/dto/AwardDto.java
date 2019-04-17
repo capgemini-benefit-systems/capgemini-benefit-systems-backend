a01f5ffdd587b388840cf635ffd48148722b81e6
jderen <kuba35groszy@tlen.pl>
2019-04-17 18:36:36 +0200

    Converter AwardDto<->Award

--- gamification/src/main/java/com/app/model/dto/AwardDto.java
+++ gamification/src/main/java/com/app/model/dto/AwardDto.java
@@ -1,5 +1,6 @@
 package com.app.model.dto;
 
+import com.app.model.Award;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -14,5 +15,26 @@ public class AwardDto {
     private String type;
     private String name;
     private String photo;
-    private Long transactionId;
+
+    /*todo*/
+    // private Long transactionId;
+
+
+    public static AwardDto getAwardDtoByAward(Award modelAward){
+        return AwardDto.builder()
+                .id(modelAward.getId())
+                .type(modelAward.getType())
+                .name(modelAward.getName())
+                .photo(modelAward.getPhoto())
+                .build();
+    }
+
+    public static Award getAwardByAwardDto(AwardDto awardDto){
+        return Award.builder()
+                .id(awardDto.getId())
+                .type(awardDto.getType())
+                .name(awardDto.getName())
+                .photo(awardDto.getPhoto())
+                .build();
+    }
 }
