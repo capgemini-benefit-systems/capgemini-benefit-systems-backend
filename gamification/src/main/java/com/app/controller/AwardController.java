0f05dcdb96868db689f4b573ca0698af0e88627d
jderen <kuba35groszy@tlen.pl>
2019-04-25 19:03:46 +0200

    AwardController findAll() and add()

--- gamification/src/main/java/com/app/controller/AwardController.java
+++ gamification/src/main/java/com/app/controller/AwardController.java
@@ -1,4 +1,4 @@
-/*package com.app.controller;
+package com.app.controller;
 
 
 import com.app.model.Award;
@@ -6,10 +6,12 @@ import com.app.model.dao.AwardDao;
 import com.app.model.dto.AwardDto;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping("/api/award")
@@ -28,8 +30,14 @@ public class AwardController {
         List <Award> awards=awardDao.findAll();
         return awards
                 .stream()
-                .map
+                .map(AwardDto::getAwardDtoByAward)
+                .collect(Collectors.toList());
+    }
+
+    @PostMapping("/add")
+    public AwardDto addAward(AwardDto awardDto){
+        Award award=AwardDto.getAwardByAwardDto(awardDto);
+        return AwardDto.getAwardDtoByAward(awardDao.insert(award));
     }
 
 }
-*/
