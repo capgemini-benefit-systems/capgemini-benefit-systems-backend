2d4cb133beb52534fa6c7ba5fe43370711403e1c
jderen <kuba35groszy@tlen.pl>
2019-06-01 20:06:25 +0200

    Repaired addAward

--- gamification/src/main/java/com/app/controller/AwardController.java
+++ gamification/src/main/java/com/app/controller/AwardController.java
@@ -5,10 +5,7 @@ import com.app.model.Award;
 import com.app.model.dao.AwardDao;
 import com.app.model.dto.AwardDto;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -35,9 +32,10 @@ public class AwardController {
     }
 
     @PostMapping("/add")
-    public AwardDto addAward(AwardDto awardDto){
+    public AwardDto addAward(@RequestBody AwardDto awardDto){
         Award award=AwardDto.getAwardByAwardDto(awardDto);
         return AwardDto.getAwardDtoByAward(awardDao.insert(award));
     }
+
 
 }
