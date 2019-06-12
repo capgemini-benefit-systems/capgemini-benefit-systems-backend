83878f3f5881ed049029f867ea1f1c1239d4c8a1
MichalPC1 <banka.michal1964@gmail.com>
2019-06-12 23:13:23 +0200

    Get project member permissions controller added

--- gamification/src/main/java/com/app/model/dao/ProjectMembersDao.java
+++ gamification/src/main/java/com/app/model/dao/ProjectMembersDao.java
@@ -4,10 +4,12 @@ import com.app.model.Project;
 import com.app.model.ProjectMembers;
 import com.app.model.User;
 import com.app.model.dao.generic.GenericDao;
+import com.app.model.enums.Permissions;
 
 import java.util.List;
 
 public interface ProjectMembersDao extends GenericDao<ProjectMembers> {
     List<Project> getProjectsByUserId(Long id);
     List<User> getUsersByProjectId(Long id);
+    Permissions getPermissionsByUserIdAndProjectId(Long userId, Long projectId);
 }
